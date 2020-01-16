8e54a01d081c9224bad23ccf11af0be6c9732222
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-16 18:09:04 +0700

    input

---
+++ input.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+puts 'Enter your name: '
+name = gets.chomp # chomp remove enter
+puts "hello #{name} you are cool"
+puts 'Enter your age: '
+age = gets.chomp
+puts "hello #{name}, you are #{age}"
