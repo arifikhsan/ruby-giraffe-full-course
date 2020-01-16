adba4e32dc0ba6f8f736eff8105dd032830e08e2
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-16 18:00:48 +0700

    numbers

---
+++ number.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+puts 5
+puts 5.8
+puts -5.8
+puts 5 + 9
+puts 6 / 3
+puts 2 ^ 3
+puts 2**3
+puts 10 % 3
+puts 5e2
+puts 5.to_s
+
+num = 20
+puts "my fav num #{num}"
+puts num.abs
+puts -12.abs
+puts -12.abs2
+puts 12.45.round # 12
+puts 12.65.round # 13
+puts 20.1.ceil # 21
+puts 20.9.floor # 20
+puts Math.sqrt(36)
+puts Math.log(1)
+puts Math.log(45)
+puts Math.cos(90)
+puts 1.0 + 7 # 8.0
+puts 10 / 7 # 1
+puts 10 / 7.0 # 1.4285
