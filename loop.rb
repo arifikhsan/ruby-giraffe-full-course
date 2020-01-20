2e6266d7aac7f8ebbbfca39dea3b8d4469db503e
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-20 10:58:25 +0700

    loop

---
+++ loop.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+friends = %w[kevin karen oscar angela andy]
+
+# for friend in friends
+#   puts friend
+# end
+
+friends.each do |friend|
+  puts friend
+end
+
+# for index in 0..5
+#   puts index
+# end
+
+(0..5).each do |index|
+  puts index
+end
+
+6.times do |index|
+  puts index
+end
